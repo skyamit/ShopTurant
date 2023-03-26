bdf86e326afa92e1815ca8da306312fb4510c9c6
Amit Kumar <amit7646014@gmail.com>
2023-03-26 23:09:38 +0530

    updated code with products controller

--- backend/ShopTurant/src/main/java/com/shopturant/ShopTurant/product/dao/ProductDao.java
+++ backend/ShopTurant/src/main/java/com/shopturant/ShopTurant/product/dao/ProductDao.java
@@ -2,7 +2,13 @@ package com.shopturant.ShopTurant.product.dao;
 
 import com.shopturant.ShopTurant.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductDao extends JpaRepository<Product, Long> {
+
+    @Query(value = "select * from product where is_active = true", nativeQuery = true)
+    public List<Product> getAllActiveProducts();
 
 }
