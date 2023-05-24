0a997fc99d328a36eae215e5841a4e8ad6ef1317
Amit Kumar <amit7646014@gmail.com>
2023-05-24 23:11:57 +0530

    Added address buttons

--- gui/shopturant/src/components/address/Address.js
+++ gui/shopturant/src/components/address/Address.js
@@ -12,12 +12,15 @@ function Address(props) {
         selectedAddress.setAddressId(event.target.value);
     }
     return (
-        <div className='d-flex p-2'>
+        <div className='d-flex p-2 addressDiv'>
             <input type="radio" name="address" value={props.data.id} checked={Number(selectedAddress.addressId) === Number(props.data.id)} onChange={updateAddressId}></input>
             <div className="b-1 p-2 m-2 singleAddressDiv">
                 <div className='singleAddressNameDiv'>{props.data.name}</div>
-                <div>{props.data.line1}, {props.data.line2}, {props.data.city}, {props.data.state}, {props.data.country}</div>
-                <div  className='singleAddressContactDiv'>{props.data.mobileNo}, {props.data.email}</div>
+                <div>{props.data.line1}, {props.data.line2}
+                    <br/> City : {props.data.city}
+                    <br/> State : {props.data.state}
+                    <br/> Country : {props.data.country}</div>
+                <div  className='singleAddressContactDiv'>Mobile No. : +91 {props.data.mobileNo}<br></br> Email-Id : {props.data.email}</div>
             </div>
         </div>
     );
--- gui/shopturant/src/components/address/EditAddress.js
+++ gui/shopturant/src/components/address/EditAddress.js
@@ -21,19 +21,27 @@ function EditAddress(props) {
         })
     }
     return (
-        <div>
-            {
-                addresses && 
+        <div className='d-flex justify-content-center flex-column align-items-center'>
+            <div className="editDiv" >
+            {   addresses && 
                 addresses.map((e)=>(
-                    <div className='d-flex p-2'>
-                        <div className="b-1 p-2 m-2 singleAddressDiv">
+                    <div className='d-flex p-2 position-relative'>
+                        <div className="b-1 p-2 m-2 singleAddressDiv position-relative mr-5">
                             <div className='singleAddressNameDiv'>{e.name}</div>
-                            <div>{e.line1}, {e.line2}, {e.city}, {e.state}, {e.country}</div>
-                            <div  className='singleAddressContactDiv'>{e.mobileNo}, {e.email}</div>
+                            <div>{e.line1}, {e.line2}
+                                <br/> City : {e.city}
+                                <br/> State : {e.state}
+                                <br/> Country : {e.country}</div>
+                            <div  className='singleAddressContactDiv'>Mobile No. : +91 {e.mobileNo}<br></br> Email-Id : {e.email}</div>
+                        </div>
+                        <div className="d-flex justify-content-center flex-column position-absolute editPosition">
+                            <button className="btn btn-warning m-2 editButton fw-bold"><img src="./pen.svg"/></button>
+                            <button className="btn btn-danger m-2 editButton fw-bold"><img src="./trash.svg"/></button>
                         </div>
                     </div>       
                 ))
             }
+            </div>
         </div>
     );
 }
